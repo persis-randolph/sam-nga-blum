281492806f27994e4299f024c5ff7743bd7328e0
persis-randolph <persis.randolph@gmail.com>
2023-11-09 09:10:14 -0600

    wip, add placeholder content for home screen

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,111 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <nav>
+      <div id="header-container">
+        <img src="logo_sam-nga-blum.svg" alt="logo" id="logo">
+        <div>
+          <RouterLink to="/" class="nav-link">HOME</RouterLink>
+          <RouterLink to="/about" class="nav-link">ABOUT</RouterLink>
+          <RouterLink to="/shop" class="nav-link">SHOP</RouterLink>
+      </div>
+      </div>
+    </nav>
   </header>
-
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+<style>
+#app {
+  width: 100vw;
 }
 
 nav {
+  position: absolute;
+  background-color: var(--color-nav-background);
+  justify-content: space-between;
+  width: 100vw;
+  height: 250px;
+  margin: 0 auto;
+  z-index: 0;
+}
+
+h1 {
+  font-family: 'DM Serif Display', serif;
+  color: #EE4623;
+  font-size: 52px;
+  margin-top: 57px;
+}
+h2 {
+  font-family: 'DM Sans', sans-serif;
+  font-size: 24px;
+  font-weight: bold;
+  letter-spacing: 0.15rem;
+  margin-bottom: 20px;
+  color: #0a0758;
+}
+p {
+  font-family: 'DM Mono', sans-serif;
+  font-size: 24px;
+  color: #0a0758;
+}
+
+#header-container {
+  max-width: 1300px;
+  margin: 0 auto;
+  justify-content: space-between;
+  display: flex;
+  padding: 50px 85px 0 85px;
+}
+
+#logo {
+  height: 45px;
+}
+
+.nav-link {
+  margin-left: 20px;
+  font-size: 18px;
+}
+
+/* header {
+  line-height: 1.5;
+  max-height: 100vh;
+} */
+
+/* .logo {
+  display: block;
+  margin: 0 auto 2rem;
+} */
+
+/* nav {
   width: 100%;
   font-size: 12px;
   text-align: center;
   margin-top: 2rem;
-}
+} */
 
-nav a.router-link-exact-active {
+/* nav a.router-link-exact-active {
   color: var(--color-text);
-}
+} */
 
-nav a.router-link-exact-active:hover {
+/* nav a.router-link-exact-active:hover {
   background-color: transparent;
-}
+} */
 
-nav a {
+/* nav a {
   display: inline-block;
   padding: 0 1rem;
   border-left: 1px solid var(--color-border);
-}
-
+} */
+/* 
 nav a:first-of-type {
   border: 0;
-}
+} */
 
-@media (min-width: 1024px) {
+/* @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
@@ -81,5 +130,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-}
+} */
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,94 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="body-wrapper">
+      <div id="top-section">
+        <h1>Hi. I&rsquo;m Sam.</h1>
+        <p>I am an Art Director, Designer, and Illustrator who loves finding creative solutions to any problem with a specialization in nonprofit branding, strategy, and communications.</p>
+      </div>
+      <div class="section">
+        <h2>RECENT WORK</h2>
+        <div id="recent-work-container">
+          <div class="placeholder-img"></div>
+          <div class="placeholder-img"></div>
+          <div class="placeholder-img"></div>
+          <div class="placeholder-img"></div>
+        </div>
+      </div>
+      <div class="section">
+        <h2>SERVICES</h2>
+        <div id="services-container">
+          <div class="services-col">
+            <div>BRANDING</div>
+            <div>PRINT/DIGITAL DESIGN</div>
+            <div>WEB DESIGN (UI/UX)</div>
+          </div>
+          <div class="services-col">
+            <div>PRODUCT/PACKAGING DESIGN</div>
+            <div>ILLUSTRATION</div>
+            <div>NONPROFIT CONSULTATION</div>
+          </div>
+        </div>
+      </div>
+      <div class="section">
+        <h2>SELECT CLIENTS</h2>
+        <div id="select-clients-container">
+          <img src="logos_select-clients.png" alt="select clients" id="client-logos">
+        </div>
+      </div>
+  </div>
   </main>
 </template>
+
+<style scoped>
+#client-logos {
+  /* object-fit: contain; */
+}
+#recent-work-container {
+  display: flex;
+  justify-content: space-between;
+}
+#services-container {
+  display: flex;
+  color: #EE4623;
+  font-family: 'DM Sans', sans-serif;
+  font-size: 20px;
+  font-weight: bold;
+  letter-spacing: 0.15rem;
+  line-height: 1.5;
+}
+#select-clients-container {
+  object-fit: contain;
+  width: 100%;
+  display: flex;
+}
+#top-section {
+  margin: 0 230px;
+  padding-bottom: 50px;
+}
+.body-wrapper {
+  margin: 150px 50px;
+  border: 1px solid #EE4623;
+  max-width: 1400px;
+  z-index: 1;
+  position: relative;
+  text-align: center;
+  min-height: 80vh;
+}
+.placeholder-img {
+  width: 280px;
+  height: 280px;
+  background-color: #F6A4B5;
+}
+.section {
+  border-top: 1px solid #EE4623;
+  text-align: left;
+  padding: 45px 35px 55px;
+}
+.services-col {
+  width: 50%;
+}
+</style>
